627958c5f64e9fce4cfba7333bbd0f312da7087f
vasukataria <vasukataria8@gmail.com>
2020-06-10 03:22:02 +0530

    27

--- vuejs/src/views/Login.vue
+++ vuejs/src/views/Login.vue
@@ -1,27 +1,28 @@
 <template>
-<div id="con">
-<form action="" method="post" class="adlog" @submit.prevent="login" enctype='multipart/form-data'>
-  <div class="imgcontainer">
-    <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
-
-  <div class="cont">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="username" required autofocus id="username" v-model="username">
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" required id="password" v-model="password">
-
-    <button class="adlog" type="submit" name="login" id="login_form">Login</button>
-    <label>
-      <input class="adlog" type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-  <div class="cont" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn" ><router-link to = '/'> Cancel </router-link></button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
+<div id="body">
+  <div id="container">
+    <div id="header">
+        <h2 id="h2">Create Account</h2>
+    </div>
+    <form id="form" action="" method="post" @submit.prevent="login" enctype='multipart/form-data'>
+        <div id="form-control">
+            <label for="username">Username</label>
+            <input type="text" placeholder="Enter your username" v-model="username" id="username" />
+            <i id="fas fa-check-circle"></i>
+            <i id="fas fa-exclamation-circle"></i>
+            <small>Error message</small>
+        </div>
+        <div id="form-control">
+            <label for="password">Password</label>
+            <input type="password" placeholder="Password" v-model="password" id="password"/>
+            <i id="fas fa-check-circle"></i>
+            <i id="fas fa-exclamation-circle"></i>
+            <small>Error message</small>
+        </div>
+        <button type="submit" name="login" id="login_form">Submit</button>
+        
+    </form>
+</div>
 </div>
 </template>
 <script>
