0fb56e2e50b5f4fcf5a30cf90a633403f882d3e4
vasukataria <vasukataria8@gmail.com>
2020-07-16 03:05:15 +0530

    router authentication

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -36,17 +36,12 @@ Vue.use(VueRouter)
     path: '/AdminHomepage',
     name: 'Homepage',
     component: AdminHomepage,
-    beforeEnter(to,from,next){
-      var isAuthenticated= false;
-    if(localStorage.getItem('LoggedUser'))
-      isAuthenticated = true;
-    else
-    isAuthenticated= false;
-    if(isAuthenticated) {
-   next(); // allow to enter route
-    } else{
-   next('/login'); // go to '/login';
-  }
+    
+    beforeRouteEnter(to, from, next) {
+    var isAuthenticated= false;
+    if(!isAuthenticated) {
+    next('/login');
+     }
     },
     children:[
     {
